71dcddbf46a07190fad1e8b41ac9b24fa6262348
aadesh1998 <aadeshmahule1998a@gmail.com>
2019-02-07 22:58:57 +0530

    First Commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+  title = 'LALITYA FOUNDATION';
+  subtitle = 'Inspired by Art.... Driven by Society....';
+}
